4641800b18639c8994fe0f27d670a65df67c8137
Emil Yusupov <mrjava3300@mail.ru>
2023-10-21 12:07:31 +0300

    pre-release version

--- src/main/python/flaskApplication.py
+++ src/main/python/flaskApplication.py
@@ -4,7 +4,17 @@ from betting_ai_project.model import *
 from webscraping.matchesScraper.matchesScraper.spiders import ligaspider
 from webscraping.matchesScraper.matchesScraper import middlewares
 
+from icecream import ic
+from bs4 import BeautifulSoup
+
+import requests
+import json
+import re
+
+
 app = Flask(import_name="subJavaFlaskApplication")
+url = "https://1xlite-769674.top/ru/live/football/"
+
 
 scrapped_data = {}
 i = len(scrapped_data)
@@ -13,14 +23,19 @@ result_data = {}
 xgb_predictor = model.MatchPredictor(model = xgb_model)
 rf_predictor = model.MatchPredictor(model = rf_model)
 
-@app.route('/scrap_data/<int:id>', methods=['GET'])
+@app.route('/scrap_data/<id>', methods=['GET'])
 def scrap_data(id):
     # scrap data from webscrapping submodule'
+    id_url = url + id
+    response = requests.get(url = id_url)
+    if (response.status_code != 200):
+        return requests.post("http::/localhost:8080/error", data=404)
+    soup = BeautifulSoup(response.text, 'lxml')
 
 
-    pass
 
-@app.route('/make_prediction/<int:id>', methods = ['GET'])
+
+@app.route('/make_prediction/<string:id>', methods = ['GET'])
 def make_prediction(id):
     # make prediction using betting_ai_project submodule
 
@@ -29,11 +44,16 @@ def make_prediction(id):
 
     result_data = xgb_predictor.predict_and_recommend(scrapped_data[id])
 
-    return
+    with open("data.json", "w") as json_file:
+        json.dump(json_file,  result_data)
 
+    int_numbers = re.findall(r'\b\d+\b', id)
 
+    int_id = int_numbers[0]
 
+    response = requests.post(f'http://localhost:8080/prediction/{int_id}', json=json_file)
 
-
-
-    pass
+    if response.status_code == 200:
+        ic("Prediction was sent to Spring")
+    else:
+        ic("Failed to send the prediction")
